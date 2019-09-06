4d0a9b780477ee617b98dd53ef7d03b876cdec3a
Hristo Uzunov <hristouzunovaz@gmail.com>
2019-09-06 23:18:50 +0300

    Initial Commit

---
+++ TankFiller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TankFiller
+//
+//  Created by Hristo Uzunov on 6.09.19.
+//  Copyright © 2019 SKLY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
